bc1389f7d1234f29f60aeab11a8c7b13252bb7f3
Tindog-FrontEnd <akshayaraj.ydg@gmail.com>
2026-06-22 16:16:48 +0530

    Add workspace files

---
+++ hd.go
@@ -0,0 +1,18 @@
+name := "John Doe"
+fmt.Println("Hello, " + name + "!")
+
+type person struct {
+	name string
+	dob string
+}
+
+user := User{
+	name: string "Jane Doe",
+	dob: string "01/01/1990",
+}
+
+fmt.Printf("Name: %s, Date of Birth: %s\n", user.name, user.dob)
+
+func add( a int, b int) int{ 
+	return a + b
+}
